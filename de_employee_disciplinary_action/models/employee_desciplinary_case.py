827f02af6e70990507bad830facdb0f764e14f47
farhanrashad <farhan@dynexcel.com>
2021-05-12 20:01:26 +0000

    update action

--- de_employee_disciplinary_action/models/employee_desciplinary_case.py
+++ de_employee_disciplinary_action/models/employee_desciplinary_case.py
@@ -3,6 +3,12 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
+READONLY_STATES = {
+    'confirm': [('readonly', True)],
+    'apply': [('readonly', True)],
+    'done': [('readonly', True)],
+    'cancel': [('readonly', True)],
+}
 
 class EmployeeDesciplinaryCaseType(models.Model):
     _name = 'hr.employee.disciplinary.action.type'
@@ -40,12 +46,13 @@ class EmployeeDesciplinaryCase(models.Model):
     
     def action_apply_penalty(self):
         self.write({
-            'state': 'close',
+            'state': 'apply',
         })
 
     state = fields.Selection([
         ('draft', 'Draft'),
         ('wait-resp', 'Waiting Response'),
+        ('apply', 'Penalty Applied'),
         ('close', 'Close'),
     ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
     name = fields.Char(string='Order Reference', readonly=True, copy=False,  index=True, default=lambda self: _('New'))
@@ -55,8 +62,13 @@ class EmployeeDesciplinaryCase(models.Model):
     user_id = fields.Many2one('res.users', string='Issuer', store=True, required=True)
     note = fields.Html(string="Description" )
 
-    apply_penalty = fields.Boolean(string='Apply Ppenalty')
-    penalty_amount = fields.Float(string='Penalty Amount')
+    apply_penalty = fields.Boolean(string='Apply Penalty')
+    penalty_amount = fields.Monetary(string='Penalty Amount', )
+    deduction_amount = fields.Monetary(string='Deduction', readonly=True, compute="_compute_all_amount")
+    residual_amount = fields.Monetary(string='Residual', readonly=True, compute="_compute_all_amount")
+    currency_id = fields.Many2one('res.currency', string='Currency', required=True, states=READONLY_STATES,
+                                  default=lambda self: self.env.user.company_id.currency_id)
+    company_id = fields.Many2one('res.company', string='Company', required=True, states=READONLY_STATES, default=lambda self: self.env.user.company_id)
     attachment_ids = fields.Many2many('ir.attachment', 'disp_case_ir_attachments_rel',
                                       'case_id', 'attachment_id', string="Attachments",
                                       help="Attach")
@@ -66,6 +78,24 @@ class EmployeeDesciplinaryCase(models.Model):
         values['name'] = seq
         res = super(EmployeeDesciplinaryCase,self).create(values)
         return res
+    
+    @api.multi
+    def _compute_all_amount(self):
+        amount = deduction = 0
+        payslips = self.env['hr.payslip']
+        input_rules = self.env['hr.payslip.input']
+
+        for adv in self:
+            amount = deduction = 0
+            payslips = self.env['hr.payslip'].search([('employee_id','=',adv.employee_id.id),('date_from','<=',adv.date),('date_to','>=', adv.date)])
+            for payslip in payslips:
+                input_rules = self.env['hr.payslip.input'].search([('payslip_id', '=', payslip.id), ('code', '=', 'EPR')])
+                for input in input_rules:
+                    deduction += input.amount
+            adv.update({
+                'deduction_amount': deduction,
+                'residual_amount': adv.penalty_amount - deduction,
+            })
 
 
     
